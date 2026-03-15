73b1d5381a32b5c9a21fc73687670700b5f87762
dab1213 <popaorlov@gmail.com>
2026-03-15 16:44:44 +0400

    com

--- src/main/java/com/nik/junit/dto/User.java
+++ src/main/java/com/nik/junit/dto/User.java
@@ -1,4 +1,10 @@
 package com.nik.junit.dto;
 
+import lombok.Value;
+
+@Value
 public class User {
+    Integer id;
+    String userName;
+    String password;
 }
--- src/test/java/com/nik/junit/service/UserServiceTest.java
+++ src/test/java/com/nik/junit/service/UserServiceTest.java
@@ -3,22 +3,51 @@ package com.nik.junit.service;
 import com.nik.junit.dto.User;
 import org.junit.jupiter.api.*;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserServiceTest {
 
+    private static final User IVAN = new User(1, "Ivan", "123");
+    private static final User PETR = new User(2, "Petr", "111");
     private UserService userService;
 
     @BeforeAll
-    static void init() {
-        System.out.println("Before all: " );
+    void init() {
+        System.out.println("Before all: " + this);
     }
 
     @BeforeEach
     void prepare() {
         userService = new UserService();
         System.out.println("Before each: " + this);
+    }
+
+    @Test
+    void loginSuccessIfUserExists(){
+        userService.add(IVAN);
+        Optional<User> masybeUser = userService.login(IVAN.getUserName(), IVAN.getPassword());
+
+        assertTrue(masybeUser.isPresent());
+        masybeUser.ifPresent(user -> assertEquals(IVAN, user));
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect(){
+        userService.add(IVAN);
+        var maybeUser = userService.login(IVAN.getUserName(), "dummy");
+
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfUserDoesNotExists(){
+        userService.add(IVAN);
+        var maybeUser = userService.login("dummy", IVAN.getPassword());
+
+        assertTrue(maybeUser.isEmpty());
     }
 
     @Test
@@ -33,8 +62,9 @@ class UserServiceTest {
     void usersSizeIfUserAdded() {
         System.out.println("Test 2: " + this);
         UserService userService = new UserService();
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(PETR);
+
         var users = userService.getAll();
         assertEquals(2, users.size());
     }
@@ -45,7 +75,7 @@ class UserServiceTest {
     }
 
     @AfterAll
-    static void closeConnectionPool() {
+    void closeConnectionPool() {
         System.out.println("After all: ");
     }
 }
